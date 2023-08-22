97d03feae063f75f4429a3e01f5a05c09afc8d2d
Joaquin Carletti <joaquin.carletti@lambdaclass.com>
2023-08-22 12:36:59 -0300

    add mul tests

--- scripts/fp12.py
+++ scripts/fp12.py
@@ -1,6 +1,8 @@
 import montgomery as monty
 import fp6
 
+FP6_ZERO = [0,0,0,0,0,0]
+
 # Algorithm 18 from https://eprint.iacr.org/2010/354.pdf
 def add(a_000, a_001, a_010, a_011, a_020, a_021, a_100, a_101, a_110, a_111, a_120, a_121, b_000, b_001, b_010, b_011, b_020, b_021, b_100, b_101, b_110, b_111, b_120, b_121):
     c0 = fp6.add(a_000, a_001, a_010, a_011, a_020, a_021, b_000, b_001, b_010, b_011, b_020, b_021)
@@ -47,27 +49,49 @@ def inv(a_000, a_001, a_010, a_011, a_020, a_021, a_100, a_101, a_110, a_111, a_
     t0 = fp6.sub(t0[0][0],t0[0][1],t0[1][0],t0[1][1],t0[2][0],t0[2][1], t2[0][0],t2[0][1],t2[1][0],t2[1][1],t2[2][0],t2[2][1])
     t1 = fp6.inv(t0[0][0],t0[0][1],t0[1][0],t0[1][1],t0[2][0],t0[2][1])
     c0 = fp6.mul(a_000, a_001, a_010, a_011, a_020, a_021,t1[0][0],t1[0][1],t1[1][0],t1[1][1],t1[2][0],t1[2][1])
-    c1 = fp6.sub(*fp6.ZERO,a_100, a_101, a_110, a_111, a_120, a_121)
+    c1 = fp6.sub(*FP6_ZERO,a_100, a_101, a_110, a_111, a_120, a_121)
     c1 = fp6.mul(c1[0][0],c1[0][1],c1[1][0],c1[1][1],c1[2][0],c1[2][1],c1[0][0],c1[0][1],c1[1][0],c1[1][1],c1[2][0],c1[2][1])
     return c0, c1
 
 def main():
 
     fp12_zero = [0 for _ in range(12)]
-    fp12_one = [monty.ONE for _ in range(12)]
-    fp12_two = [monty.TWO for _ in range(12)]
+    fp12_one = [1] + [0 for _ in range(11)]
+    fp12_all_one = [monty.ONE for _ in range(12)]
+    fp12_all_two = [monty.TWO for _ in range(12)]
 
-    #ADDITION
-    assert(add(*fp12_zero, *fp12_zero) == fp12_zero)
-    assert(add(*fp12_zero, *fp12_one) == fp12_one)
-    assert(add(*fp12_one, *fp12_zero) == fp12_one)
-    assert(add(*fp12_one, *fp12_one) == fp12_two)
+    # ADDITION
+    assert(add(*fp12_zero, *fp12_zero) == (((0, 0), (0, 0), (0, 0)), ((0, 0), (0, 0), (0, 0))))
+    assert(add(*fp12_zero, *fp12_all_one) == (((monty.ONE, monty.ONE), (monty.ONE, monty.ONE), (monty.ONE, monty.ONE)), ((monty.ONE, monty.ONE), (monty.ONE, monty.ONE), (monty.ONE, monty.ONE))))
+    assert(add(*fp12_all_one, *fp12_zero) == (((monty.ONE, monty.ONE), (monty.ONE, monty.ONE), (monty.ONE, monty.ONE)), ((monty.ONE, monty.ONE), (monty.ONE, monty.ONE), (monty.ONE, monty.ONE))))
+    assert(add(*fp12_all_one, *fp12_all_one) == (((monty.TWO, monty.TWO), (monty.TWO, monty.TWO), (monty.TWO, monty.TWO)), ((monty.TWO, monty.TWO), (monty.TWO, monty.TWO), (monty.TWO, monty.TWO))))
 
-    #SUBTRACTION
-    assert(sub(*fp12_zero, *fp12_zero) == fp12_zero)
-    assert(sub(*fp12_two, *fp12_one) == fp12_one)
-    assert(sub(*fp12_one, *fp12_zero) == fp12_one)
-    assert(sub(*fp12_one, *fp12_one) == fp12_zero)
+    # SUBTRACTION
+    assert(sub(*fp12_zero, *fp12_zero) == (((0, 0), (0, 0), (0, 0)), ((0, 0), (0, 0), (0, 0))))
+    assert(sub(*fp12_all_two, *fp12_all_one) == (((monty.ONE, monty.ONE), (monty.ONE, monty.ONE), (monty.ONE, monty.ONE)), ((monty.ONE, monty.ONE), (monty.ONE, monty.ONE), (monty.ONE, monty.ONE))))
+    assert(sub(*fp12_all_one, *fp12_zero) == (((monty.ONE, monty.ONE), (monty.ONE, monty.ONE), (monty.ONE, monty.ONE)), ((monty.ONE, monty.ONE), (monty.ONE, monty.ONE), (monty.ONE, monty.ONE))))
+    assert(sub(*fp12_all_one, *fp12_all_one) == (((0, 0), (0, 0), (0, 0)), ((0, 0), (0, 0), (0, 0))))
+
+    # MULTIPLICATION BY 0
+    assert(mul(*fp12_zero, *fp12_zero)) == (((0, 0), (0, 0), (0, 0)), ((0, 0), (0, 0), (0, 0)))
+    assert(mul(*fp12_all_one, *fp12_zero)) == (((0, 0), (0, 0), (0, 0)), ((0, 0), (0, 0), (0, 0)))
+    assert(mul(*fp12_zero, *fp12_all_two)) == (((0, 0), (0, 0), (0, 0)), ((0, 0), (0, 0), (0, 0)))
+
+    # MULTIPLICATION BY 1
+    print(mul(*fp12_all_one, *fp12_one))
+    print(mul(*fp12_one, *fp12_all_two))
+    # assert(mul(*fp12_all_one, *fp12_one)) == (((monty.ONE, monty.ONE), (monty.ONE, monty.ONE), (monty.ONE, monty.ONE)), ((monty.ONE, monty.ONE), (monty.ONE, monty.ONE), (monty.ONE, monty.ONE)))
+    # assert(mul(*fp12_one, *fp12_all_two)) == (((monty.TWO, monty.TWO), (monty.TWO, monty.TWO), (monty.TWO, monty.TWO)), ((monty.TWO, monty.TWO), (monty.TWO, monty.TWO), (monty.TWO, monty.TWO)))
+
+    # MULTIPLY BY INVERSE
+    fp12_all_one_inverse = inv(*fp12_all_one)
+    fp12_all_two_inverse = inv(*fp12_all_two)
+    multiplication = mul(
+        *fp12_all_one_inverse[0][0],*fp12_all_one_inverse[0][1],*fp12_all_one_inverse[0][2],*fp12_all_one_inverse[1][0],*fp12_all_one_inverse[1][1],*fp12_all_one_inverse[1][2],
+        *fp12_all_two_inverse[0][0],*fp12_all_two_inverse[0][1],*fp12_all_two_inverse[0][2],*fp12_all_two_inverse[1][0],*fp12_all_two_inverse[1][1],*fp12_all_two_inverse[1][2]
+    )
+    print(multiplication)
+    # assert(multiplication == (((1, 0), (0, 0), (0, 0)), ((0, 0), (0, 0), (0, 0))))
 
 if __name__ == '__main__':
     main()
