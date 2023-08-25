c698633467d661d429b8d51c52e7d6fb094beb2b
Joaquin Carletti <56092489+ColoCarletti@users.noreply.github.com>
2023-08-25 11:58:59 -0300

    Point doubling and line evaluation (#25)

---
+++ scripts/point_doubling_and_line_evaluation.py
@@ -0,0 +1,55 @@
+import fp2
+import montgomery as monty
+
+# Algorithm 26. https://eprint.iacr.org/2010/354.pdf
+# P belongs to curve E over Fp in affine coordinates: P = (xp, yp)
+# Q belongs to curve E' over Fp2 in Jacobian coordinates: Q = (Xq, Yq, Zq)
+def point_doubling_and_line_evaluation(xp, yp, Xq0, Xq1, Yq0, Yq1, Zq0, Zq1):
+    t0 = fp2.mul(Xq0,Xq1,Xq0,Xq1)
+    t1 = fp2.mul(Yq0,Yq1,Yq0,Yq1)
+    t2 = fp2.mul(*t1,*t1)
+    # TODO: This could be an optimization in the future, make sure to test it
+    # t3 = fp2.mul(*t1,Xq0,Xq1)
+    # t3 = fp2.add(*t3, *t3)
+    t3 = fp2.add(*t1,Xq0,Xq1)
+    t3 = fp2.mul(*t3,*t3)
+    t3 = fp2.sub(*t3,*t0)
+    t3 = fp2.sub(*t3,*t2)
+    t3 = fp2.add(*t3,*t3)
+    t4 = fp2.scalar_mul(*t0,monty.THREE)
+    t6 = fp2.add(Xq0,Xq1,*t4)
+    t5 = fp2.mul(*t4,*t4)
+    Xt = fp2.scalar_mul(*t3,2)
+    Xt = fp2.sub(*t5,*Xt)
+    Zq_squared = fp2.mul(Zq0,Zq1,Zq0,Zq1)
+    # TODO: This could be an optimization in the future, make sure to test it
+    # Zt = fp2.mul(Yq0,Yq1,Zq0,Zq1 )
+    # Zt = fp2.add(*Zt, *Zt)
+    Zt = fp2.add(Yq0,Yq1,Zq0,Zq1)
+    Zt = fp2.mul(*Zt,*Zt)
+    Zt = fp2.sub(*Zt,*t1)
+    Zt = fp2.sub(*Zt,Zq_squared)
+    t2_times_eight = fp2.scalar_mul(*t2,monty.EIGHT)
+    Yt = fp2.sub(*t3,*Xt)
+    Yt = fp2.mul(*Yt,*t4)
+    Yt = fp2.sub(*Yt,*t2_times_eight)
+    t3 = fp2.mul(*Zq_squared,*t4)
+    t3 = fp2.add(*t3,*t3)
+    t3 = fp2.sub(0,0,*t3) # multiply by -1
+    t3 = fp2.scalar_mul(*t3,xp)
+    t1_times_4 = fp2.scalar_mul(*t1,monty.FOUR)
+    t6 = fp2.mul(*t6,*t6)
+    t6 = fp2.sub(*t6,*t0)
+    t6 = fp2.sub(*t6,*t5)
+    t6 = fp2.sub(*t6,*t1_times_4)
+    t0 = fp2.mul(*Zt,*Zq_squared)
+    t0 = fp2.add(*t0,*t0)
+    t0 = fp2.scalar_mul(*t0,yp)
+    T = (Xt, Yt, Zt)
+    l = (*t3,0,0,0,0,*t3,*t6,0,0)
+    return l, T
+
+
+
+
+
