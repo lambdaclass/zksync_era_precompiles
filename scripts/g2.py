5147bc38b7db8f1223e1c3cb281f67e3af5de1ec
Joaquin Carletti <joaquin.carletti@lambdaclass.com>
2023-08-28 14:00:50 -0300

    fix return of negate funciton

--- scripts/g2.py
+++ scripts/g2.py
@@ -2,7 +2,7 @@ import fp2
 import montgomery as monty
 
 def neg(x0,x1, y0, y1, z0, z1):
-    y0 = fp2.neg(y0,y1)
+    y0,y1= fp2.neg(y0,y1)
     return (x0,x1,y0,y1,z0,z1)
 
 def from_affine(x0,x1,y0,y1):
