29a19279665dd52c617b9e35533e4bc319f0251b
IAvecilla <iavecilla@fi.uba.ar>
2023-08-30 18:59:38 -0300

    Add conditional for g2 from affine

--- scripts/g2.py
+++ scripts/g2.py
@@ -6,6 +6,9 @@ def neg(x0,x1, y0, y1, z0, z1):
     return (x0,x1,y0,y1,z0,z1)
 
 def from_affine(x0,x1,y0,y1):
+    if x0 == 0 and x1 == 0 and y0 == 0 and y1 == 0:
+        return (monty.ONE, 0, monty.ONE, 0, 0, 0)
+    
     z0 = monty.ONE
     z1 = 0
     return (x0,x1,y0,y1,z0,z1)
