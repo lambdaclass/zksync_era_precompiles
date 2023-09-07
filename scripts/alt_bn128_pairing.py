7c71f00b752a782dd1ba325aeb18eb1351638a35
IAvecilla <iavecilla@fi.uba.ar>
2023-09-07 16:37:40 -0300

    Delete unnecesary comments

--- scripts/alt_bn128_pairing.py
+++ scripts/alt_bn128_pairing.py
@@ -47,44 +47,43 @@ def point_doubling_and_line_evaluation(Xq0, Xq1, Yq0, Yq1, Zq0, Zq1):
     return l,T
 
 def point_addition_and_line_evaluation(Xq0, Xq1, Yq0, Yq1, Xt0, Xt1, Yt0, Yt1, Zt0, Zt1):
-    temp = fp2.mul(Yq0,Yq1,Zt0,Zt1) # Y2Z1.Mul(&a.Y, &p.z)
-    O = fp2.sub(Yt0,Yt1,*temp) # O.Sub(&p.y, &Y2Z1)
-    temp = fp2.mul(Xq0,Xq1,Zt0,Zt1) # X2Z1.Mul(&a.X, &p.z)
-    L = fp2.sub(Xt0,Xt1,*temp) # L.Sub(&p.x, &X2Z1)
-    C = fp2.mul(*O,*O) # C.Square(&O)
-    D = fp2.mul(*L,*L) # D.Square(&L)
-    E = fp2.mul(*L,*D) # E.Mul(&L, &D)
-    F = fp2.mul(Zt0,Zt1,*C) # F.Mul(&p.z, &C)
-    G = fp2.mul(Xt0,Xt1,*D) # G.Mul(&p.x, &D)
-    temp = fp2.add(*G,*G) # t0.Double(&G)
+    temp = fp2.mul(Yq0,Yq1,Zt0,Zt1) 
+    O = fp2.sub(Yt0,Yt1,*temp)
+    temp = fp2.mul(Xq0,Xq1,Zt0,Zt1)
+    L = fp2.sub(Xt0,Xt1,*temp)
+    C = fp2.mul(*O,*O)
+    D = fp2.mul(*L,*L)
+    E = fp2.mul(*L,*D)
+    F = fp2.mul(Zt0,Zt1,*C)
+    G = fp2.mul(Xt0,Xt1,*D)
+    temp = fp2.add(*G,*G)
     H = fp2.add(*E,*F)
-    H = fp2.sub(*H,*temp) # H.Add(&E, &F).Sub(&H, &t0)
-    temp = fp2.mul(Yt0, Yt1, *E) # t1.Mul(&p.y, &E)
+    H = fp2.sub(*H,*temp) 
+    temp = fp2.mul(Yt0, Yt1, *E) 
 
     # X, Y, Z
-    Tx0, Tx1 = fp2.mul(*L,*H) # p.x.Mul(&L, &H)
+    Tx0, Tx1 = fp2.mul(*L,*H)
     Ty0, Ty1 = fp2.sub(*G,*H)
     Ty0, Ty1 = fp2.mul(Ty0,Ty1,*O)
-    Ty0, Ty1 = fp2.sub(Ty0,Ty1,*temp) # p.y.Sub(&G, &H).Mul(&p.y, &O).Sub(&p.y, &t1)
-    Tz0, Tz1 = fp2.mul(*E, Zt0, Zt1) # p.z.Mul(&E, &p.z)
+    Ty0, Ty1 = fp2.sub(Ty0,Ty1,*temp)
+    Tz0, Tz1 = fp2.mul(*E, Zt0, Zt1)
 
-    temp = fp2.mul(*L,Yq0,Yq1) # t2.Mul(&L, &a.Y)
+    temp = fp2.mul(*L,Yq0,Yq1)
     J = fp2.mul(Xq0,Xq1,*O) 
-    J = fp2.sub(*J, *temp) # J.Mul(&a.X, &O).Sub(&J, &t2)
+    J = fp2.sub(*J, *temp)
     
     # Line evaluation
-    l0 = L # evaluations.r0.Set(&L)
-    l1 = fp2.neg(*O) # evaluations.r1.Neg(&O)
-    l2 = J # evaluations.r2.Set(&J)
+    l0 = L
+    l1 = fp2.neg(*O)
+    l2 = J
 
     l = (*l0,0,0,0,0,*l1,*l2,0,0)
     T = Tx0, Tx1, Ty0, Ty1, Tz0, Tz1
 
     return l, T
 
-# Algorithm 31 from https://eprint.iacr.org/2010/354.pdf
+# Algorithm 6 from https://eprint.iacr.org/2015/192.pdf
 def final_exponentiation(a_000, a_001, a_010, a_011, a_020, a_021, a_100, a_101, a_110, a_111, a_120, a_121):
-    
     f = (a_000, a_001, a_010, a_011, a_020, a_021, a_100, a_101, a_110, a_111, a_120, a_121)
     
     # First part
@@ -190,8 +189,6 @@ def miller_loop(Xq0, Xq1, Yq0, Yq1, xp, yp):
 
 def pair(xp, yp, Xq0, Xq1, Yq0, Yq1):
     f = miller_loop(Xq0, Xq1, Yq0, Yq1, xp, yp)
-    # This should be final exponentiation
-    # f = fp12.exponentiation(*f, 552484233613224096312617126783173147097382103762957654188882734314196910839907541213974502761540629817009608548654680343627701153829446747810907373256841551006201639677726139946029199968412598804882391702273019083653272047566316584365559776493027495458238373902875937659943504873220554161550525926302303331747463515644711876653177129578303191095900909191624817826566688241804408081892785725967931714097716709526092261278071952560171111444072049229123565057483750161460024353346284167282452756217662335528813519139808291170539072125381230815729071544861602750936964829313608137325426383735122175229541155376346436093930287402089517426973178917569713384748081827255472576937471496195752727188261435633271238710131736096299798168852925540549342330775279877006784354801422249722573783561685179618816480037695005515426162362431072245638324744480)
     f = final_exponentiation(*f)
     return f
 
@@ -219,11 +216,9 @@ def main():
     Xq1 = monty.into(14752851163271972921165116810778899752274893127848647655434033030151679466487)
     Yq0 = monty.into(8159591693044959083845993640644415462154314071906244874217244895511876957520)
     Yq1 = monty.into(19774899457345372253936887903062884289284519982717033379297427576421785416781)
-    # Zq0 = monty.ONE
-    # Zq1 = 0
 
-    # assert(utils.is_in_curve(xp0, yp0))
-    # assert(utils.is_in_twisted_curve(Xq0, Xq1, Yq0, Yq1))
+    assert(utils.is_in_curve(xp0, yp0))
+    assert(utils.is_in_twisted_curve(Xq0, Xq1, Yq0, Yq1))
 
     xp1 = monty.into(7742452358972543465462254569134860944739929848367563713587808717088650354556)
     yp1 = monty.into(14563720768440487558151020426243236708567496944263114635856508834497000371217)
@@ -231,15 +226,12 @@ def main():
     Xt1 = monty.into(11559732032986387107991004021392285783925812861821192530917403151452391805634)
     Yt0 = monty.into(8495653923123431417604973247489272438418190587263600148770280649306958101930)
     Yt1 = monty.into(4082367875863433681332203403145435568316851327593401208105741076214120093531)
-    # Zt0 = monty.ONE
-    # Zt1 = 0                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             6170940445994484564222204938066213705353407449799250191249554538140978927342]]
 
-    # assert(utils.is_in_curve(xp1, yp1))
-    # assert(utils.is_in_twisted_curve(Xt0, Xt1, Yt0, Yt1))
+    assert(utils.is_in_curve(xp1, yp1))
+    assert(utils.is_in_twisted_curve(Xt0, Xt1, Yt0, Yt1))
 
 
     # Pairing Test
-    # Should be 1
     a = pair(xp0, yp0, Xq0, Xq1, Yq0, Yq1)
     b = pair(xp1, yp1, Xt0, Xt1, Yt0, Yt1)
     result = fp12.mul(*a, *b)
