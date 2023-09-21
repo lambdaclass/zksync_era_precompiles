56501f06a221b7a839a0c862edaf1a47788e0f88
Francisco Krause Arnim <fkrausear@gmail.com>
2023-09-21 17:13:23 -0300

    Add barret script

---
+++ scripts/barret_reduction.py
@@ -0,0 +1,56 @@
+import math, unittest, random, pdb
+
+
+def modular_exponentiation(base: int, exponent: int, modulus: int) -> int:
+    r, k = calculate_r_constant_for(modulus)
+    result = 1
+    base = base % modulus
+
+    while exponent > 0:
+        if exponent % 2 == 1:
+            result = barrett_reduction(result * base, modulus)
+        base = barrett_reduction(base * base, modulus)
+        exponent >>= 1
+    return result
+
+def log2_for_n(n: int) -> int:
+    result = 0.
+    while n >= 1:
+       n = n >> 2 
+       result = result + 1
+    return result
+
+# The r constant for Barret is r = 4**k / n. 
+# Where k = log2(n), and n the modulo.
+def calculate_r_constant_for(n: int) -> (int, int):
+   # Since 4**k = (2**(2**k))
+   # we can shift 1 2k times instead.
+    k = int((log2_for_n(n)) % n)
+    # 4**k
+    four_to_the_power_of_k = (1 << 2*k)
+    # Seems like this division cannot be avoided
+    r = four_to_the_power_of_k // n
+    return r,k
+
+# Barret reduction, taking this blog post as reference:
+# https://www.nayuki.io/page/montgomery-reduction-algorithm
+def barrett_reduction(x: int, n: int) -> int:
+    assert 0 <= x <= n**2
+    r,k = calculate_r_constant_for(n)
+    t = x - (((x*r)) >> (2*k))*n
+    if t < n:
+        reduction = t
+    else:
+        reduction = t - n
+    return reduction
+
+class BarretTester(unittest.TestCase):
+	def test_basic(self) -> None:
+		for _ in range(3000):
+                    x = random.randint(1, 2**64)
+                    modulo: int = random.randrange(x**2, x**3)
+                    expected: int = x % modulo
+                    self.assertEqual(expected, barrett_reduction(x, modulo)) 
+
+if __name__ == '__main__':
+    unittest.main()
