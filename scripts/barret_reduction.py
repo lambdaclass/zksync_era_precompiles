1d3a44217cf1e36da10f6e8b816c685e9e6cf6f5
Francisco Krause Arnim <fkrausear@gmail.com>
2023-09-21 18:55:20 -0300

    Add tests barret reduction

--- scripts/barret_reduction.py
+++ scripts/barret_reduction.py
@@ -1,17 +1,17 @@
 import math, unittest, random, pdb
 
 
-def modular_exponentiation(base: int, exponent: int, modulus: int) -> int:
-    r, k = calculate_r_constant_for(modulus)
-    result = 1
-    base = base % modulus
+# def modular_exponentiation(base: int, exponent: int, modulus: int) -> int:
+#     r, k = calculate_r_constant_for(modulus)
+#     result = 1
+#     base = base % modulus
 
-    while exponent > 0:
-        if exponent % 2 == 1:
-            result = barrett_reduction(result * base, modulus)
-        base = barrett_reduction(base * base, modulus)
-        exponent >>= 1
-    return result
+#     while exponent > 0:
+#         if exponent % 2 == 1:
+#             result = barrett_reduction(result * base, modulus)
+#         base = barrett_reduction(base * base, modulus)
+#         exponent >>= 1
+#     return result
 
 def log2_for_n(n: int) -> int:
     result = 0.
@@ -44,13 +44,30 @@ def barrett_reduction(x: int, n: int) -> int:
         reduction = t - n
     return reduction
 
+def modular_exponentiation(base: int, exponent: int, modulus: int) -> int:
+    result = 1
+    # base = barrett_reduction(base, modulus) 
+    while exponent > 0:
+        if exponent % 2 == 1:
+            result = barrett_reduction(result*base, modulus)
+        exponent = exponent >> 1
+        base = barrett_reduction(base * base, modulus)
+    return result
+
 class BarretTester(unittest.TestCase):
-	def test_basic(self) -> None:
-		for _ in range(3000):
+        def test_basic(self) -> None:
+                for _ in range(300):
                     x = random.randint(1, 2**64)
                     modulo: int = random.randrange(x**2, x**3)
                     expected: int = x % modulo
                     self.assertEqual(expected, barrett_reduction(x, modulo)) 
+        def test_modular_exponentiation(self) -> None:
+            for _ in range(100):
+                exponent = random.randint(1, 5)
+                base = random.randint(1, 2**10)
+                modulo: int = random.randrange(base**exponent, base**(exponent+1))
+                expected = (base**exponent)%modulo
+                result = modular_exponentiation(base, exponent, modulo)
 
 if __name__ == '__main__':
     unittest.main()
