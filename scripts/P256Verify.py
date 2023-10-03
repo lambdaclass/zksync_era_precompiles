6a4023b654cf9b1625446ffd205beded1ec5860b
Joaquin Carletti <joaquin.carletti@lambdaclass.com>
2023-10-03 18:47:34 -0300

    fix letra

--- scripts/P256Verify.py
+++ scripts/P256Verify.py
@@ -75,15 +75,14 @@ def main():
     gx = 0x6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296
     gy = 0x4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5
 
-
     x,y = escalarMul((gx,gy),k)
     r = x % n
     assert r != 0
+
     k_inv = pow(k, n-2, n)
-    s = (k_inv * (z + k * da)) % n
+    s = (k_inv * (z + r * da)) % n
 
     s_inv = pow(s, n-2, n)
-
     assert s_inv * s % n == 1
 
     u1 = (z * s_inv) % n
