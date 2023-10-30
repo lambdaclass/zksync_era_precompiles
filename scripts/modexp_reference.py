7dd345a0ed08474f2a28697dc62fdcc74a3fe1eb
Francisco Krause Arnim <fkrausear@gmail.com>
2023-10-30 14:52:53 -0300

    Update python script to debug yul

--- scripts/modexp_reference.py
+++ scripts/modexp_reference.py
@@ -7,8 +7,10 @@ def modular_pow(base, exponent, modulus):
 
     result = 1
     base %= modulus
-
+    
     while exponent > 0:
+        print(f"face: {hex(result)}")
+        print(f"dead: {hex(exponent)}")
         if exponent % 2 == 1:
             result = (result * base) % modulus
         exponent >>= 1
@@ -16,10 +18,10 @@ def modular_pow(base, exponent, modulus):
     return result
 
 # Example usage:
-base = 4
-exponent = 2
-modulus = 3
+base = 3
+exponent = 115792089237316195423570985008687907853269984665640564039457584007908834671662
+modulus = 115792089237316195423570985008687907853269984665640564039457584007908834671663
 result = modular_pow(base, exponent, modulus)
-expected = (base**exponent) % modulus
+expected = (pow(base, exponent, modulus)) 
 print(f"Expected: {expected}, got: {result}")
 assert result == expected
