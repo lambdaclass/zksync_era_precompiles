264409cd56cb836b75714b6542efb732ff7abc75
Iván Litteri <ilitteri@fi.uba.ar>
2023-11-03 18:21:59 -0300

    Implement naive compile_zkvyper

--- tests/src/compile/compiler.rs
+++ tests/src/compile/compiler.rs
@@ -11,6 +11,7 @@ use zksync_web3_rs::{
 pub enum Compiler {
     ZKSolc,
     Solc,
+    ZKVyper,
 }
 
 // Unwrap is allowed here because of the trait.
@@ -66,6 +67,7 @@ pub fn compile(
     match compiler {
         Compiler::ZKSolc => compile_with_zksolc(project_root, contract_path, contract_name),
         Compiler::Solc => compile_with_solc(contract_path, contract_name),
+        Compiler::ZKVyper => Ok(compile_with_zkvyper(project_root, contract_path, contract_name)),
     }
 }
 
@@ -107,11 +109,71 @@ fn compile_with_solc(contract_path: &str, contract_name: &str) -> eyre::Result<A
     Ok(Artifact::SolcArtifact(artifact))
 }
 
+fn compile_with_zkvyper(
+    project_root: &str,
+    contract_path: &str,
+    contract_name: &str,
+) -> Artifact {
+    let root = PathBuf::from(project_root);
+    let zk_project = ZKSProject::from(
+        Project::builder()
+            .paths(ProjectPathsConfig::builder().build_with_root(root))
+            .set_auto_detect(true)
+            .build()
+            .unwrap(),
+    );
+    let zkvyper_path = dirs::config_dir()
+        .unwrap()
+        .join("eth-compilers")
+        .join("zkvyper");
+    let vyper_path = dirs::config_dir()
+        .unwrap()
+        .join("eth-compilers")
+        .join("vyper");
+
+    let command = &mut std::process::Command::new(zkvyper_path);
+    command
+        .arg("-f")
+        .arg("combined_json")
+        .arg("--vyper")
+        .arg(vyper_path)
+        .arg(zk_project.base_project.root());
+
+    let command_output = command.output().unwrap();
+
+    let compilation_output = String::from_utf8_lossy(&command_output.stdout)
+            .into_owned()
+            .trim()
+            .to_owned();
+    
+    let parsed_compilation_output = serde_json::Value::from_str(&compilation_output).unwrap();
+
+    let abi: Abi = serde_json::from_value(parsed_compilation_output[contract_path]["abi"].clone()).unwrap();
+    let bin: Bytes = serde_json::from_value(parsed_compilation_output[contract_path]["bytecode"].clone()).unwrap();
+
+    let zks_artifact = ZKSArtifact {
+        abi: Some(abi),
+        bin: Some(bin),
+        metadata: None,
+        devdoc: None,
+        userdoc: None,
+        storage_layout: None,
+        ast: None,
+        asm: None,
+        bin_runtime: None,
+        hashes: None,
+        factory_deps: None,
+    };
+
+    Artifact::ZKSArtifact(zks_artifact)
+}
+
 #[allow(dead_code)]
 pub fn build(contract_path: &str, compiler: Compiler) -> eyre::Result<()> {
     match compiler {
         Compiler::ZKSolc => build_with_zksolc(contract_path),
         Compiler::Solc => build_with_solc(contract_path),
+        Compiler::ZKVyper => todo!(),
     }
 }
 
